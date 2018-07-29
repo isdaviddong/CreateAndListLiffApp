d6c7d4d0108fc62c42ba31ecc223a091a77834b2
isdaviddong <isdaviddong@hotmail.com>
2018-07-29 12:45:32 +0800

    v1

---
+++ WebApplication1/default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="Button1" runat="server" Text="Add Liff App" OnClick="Button1_Click" />
+        <br />
+                <asp:Button ID="Button2" runat="server" Text="List Liff Apps" OnClick="Button2_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        const string ChannelAccessToken = "請換成你自己的Channel Access Token";
+        const string AdminUserId = "請換成你自己的Admin User Id";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+       
+            var LiffURL = "https://testliff.azurewebsites.net/default.html"; //測試用位置
+
+            //建立LiffApp
+            var Liff = isRock.LIFF.Utility.AddLiffApp(
+                ChannelAccessToken, new Uri(LiffURL), isRock.LIFF.ViewType.tall);
+            //顯示建立好的 Liff App
+            isRock.LineBot.Utility.PushMessage(
+                AdminUserId, "Liff App 已新增 " + Liff, ChannelAccessToken);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //取得特定ChannelAccessToken名下所有的Liff Apps
+            var Liff = isRock.LIFF.Utility.GetAllLiffApps(ChannelAccessToken);
+            //列舉所有Liff Apps
+            foreach (var item in Liff.apps)
+            {
+                Response.Write($"<br>liffId : {item.liffId}");
+            }
+        }
+    }
+}
